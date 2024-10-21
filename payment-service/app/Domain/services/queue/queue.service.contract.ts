cdd8560aa457fe6c65ba253cc43470ad541cc960
josé wilson <wilsonfilho.dev@gmail.com>
2024-10-21 03:14:07 -0300

    Payment-service removing unused import

--- payment-service/app/Domain/services/queue/queue.service.contract.ts
+++ payment-service/app/Domain/services/queue/queue.service.contract.ts
@@ -1,5 +1,4 @@
 import { InputQueueServiceDto } from 'App/Application/dtos/queue/queue-service.dto';
-import { SQSEvent, SQSHandler } from 'aws-lambda';
 
 export interface QueueServiceContract {
     send(data: InputQueueServiceDto): Promise<void>
